2757f9846dceebdcfbe12866cb7585b261dfd5d8
icarojf <icarojf@gmail.com>
2014-08-06 17:44:59 -0500

    lista de Eventos

--- Producto/platforms/android/assets/www/js/scripts.js
+++ Producto/platforms/android/assets/www/js/scripts.js
@@ -2,15 +2,17 @@
 var myLocation = new google.maps.LatLng(5.067132,-75.51828799999998);
 var servidorUrl = 'http://sportivas.com.co/';
 
-var map, mapEscenarios;
+var map, mapEscenarios, mapEventos;
 var markers = [];
 
 var escenariosCiudad = [];// = ['Bosque popular', 'Coliseo mayor', 'Coliseo menor', 'Club Manizales', 'Escenario 1'];
+var eventosCiudad = [];// = ['Bosque popular', 'Coliseo mayor', 'Coliseo menor', 'Club Manizales', 'Escenario 1'];
 
 
  $(document).ready(function () {
 	google.maps.event.addDomListener(window, 'load', inicializarMapa);
 	google.maps.event.addDomListener(window, 'load', listarEscenarios);
+	google.maps.event.addDomListener(window, 'load', listarEventos);
 	
 	$('.nav a').on('click', function(){
 		//$(".btn-navbar").click(); //bootstrap 2.x
@@ -28,6 +30,13 @@ var escenariosCiudad = [];// = ['Bosque popular', 'Coliseo mayor', 'Coliseo meno
 		escenariosCiudad[indice] = dato.nombre;
  	});
 	
+	var listaEventosCiudad = '[{"id":"2","titulo":"Torneo de Ultimate Femenino","fecha":"2014-08-22","hora":"10:00","descripcion":"Torneo Femenino de Ultimate que se llevar\u00e1 a cabo en las instalaciones del bosque popular cerca a la cafeter\u00eda.","telefono":"8890000","email":"contacto@correo.com","escenario-nombre":"Bosque Popular","escenario-latitud":"5.036146","escenario-longitud":"-75.473853","escenario-direccion":"Cra 31 Via Enea"},{"id":"1","titulo":"Caminata Campa\u00f1a Contra el Cancer de mama","fecha":"2014-08-08","hora":"09:00","descripcion":"Marcha para tomar conciencia de los cuidados para combatir el c\u00e1ncer de mama.","telefono":"8890000","email":"contacto@correo.com","escenario-nombre":"Coliseo Mayor","escenario-latitud":"5.057593","escenario-longitud":"-75.487366","escenario-direccion":"Cra 23 # 63 - 18"}]';
+	listaEventosCiudad =eval(listaEventosCiudad);
+	
+	$.each(listaEventosCiudad, function (indice, dato) {	
+		eventosCiudad[indice] = dato.titulo;
+ 	});
+	
 	
 	// Workaround for bug in mouse item selection
 	$.fn.typeahead.Constructor.prototype.blur = function() {
@@ -43,8 +52,9 @@ var escenariosCiudad = [];// = ['Bosque popular', 'Coliseo mayor', 'Coliseo meno
 	
 	$('#escenarios_busqueda').on('change', function(event) {
 		clearMarkers();
-		setInMap(map, event.target.value);
+		setInMap(map, event.target.value, escenariosCiudad);
 	});
+	
 	
 	$('#nombreEscenarioUno_busqueda').typeahead({
 		source: function(query, process) {
@@ -54,7 +64,18 @@ var escenariosCiudad = [];// = ['Bosque popular', 'Coliseo mayor', 'Coliseo meno
 	
 	$('#nombreEscenarioUno_busqueda').on('change', function(event) {
 		clearMarkers();
-		setInMap(mapEscenarios, event.target.value);
+		setInMap(mapEscenarios, event.target.value, escenariosCiudad);
+	});
+	
+	$('#nombreEventoUno_busqueda').typeahead({
+		source: function(query, process) {
+		return eventosCiudad;
+		}
+	});
+	
+	$('#nombreEventoUno_busqueda').on('change', function(event) {
+		clearMarkers();
+		setInMap(mapEventos, event.target.value, eventosCiudad);
 	});
   
 });
@@ -79,6 +100,13 @@ function abrirPanelEventos(){
 	google.maps.event.trigger(map, 'resize');
 }
 
+function abrirPanelEventosBusqueda(){
+	cerrarPaneles();
+	listarEventos();
+	$("#panelEventosBusqueda").show();
+	google.maps.event.trigger(mapEventos, 'resize');
+}
+
 function abrirPanelEscenarios(){
 	cerrarPaneles();
 	listarEscenarios();	
@@ -86,10 +114,16 @@ function abrirPanelEscenarios(){
 	google.maps.event.trigger(mapEscenarios, 'resize');
 }
 
+function abrirPanelLogin(){
+	cerrarPaneles();
+	$("#panelLogin").show();
+}
+
 function cerrarPaneles(){
 	$("#panelLogin").hide();
 	$("#panelMuro").hide();
 	$("#panelEvento").hide();
+	$("#panelEventoBusqueda").hide();
 	$("#panelRegistrarDeportes").hide();
 	$("#panelEscenarios").hide();
 }
@@ -164,7 +198,7 @@ $( document ).ready(function() {
 /*********** MAPAS *****************/
 /*Mapa*/
 function inicializarMapa() {
-
+	markers = [];
 	geocoder = new google.maps.Geocoder();
 
      var address = "manizales";
@@ -219,9 +253,9 @@ function toggleBounce() {
 
 
 // Sets the map on all markers in the array.
-function setInMap(mapAux, value) {
+function setInMap(mapAux, value, array) {
   for (var i = 0; i < markers.length; i++) {
-	if(escenariosCiudad[i]==value){
+	if(array[i]==value){
 		markers[i].setMap(mapAux);
 	}
   }
@@ -249,7 +283,7 @@ function showMarkers() {
   
   /*listar escenarios*/
   function listarEscenarios(){
-  
+	markers = [];
 	//servicio retorna este json
 	var listaEscenarios = '[{"id":"2","nombre":"Bosque Popular","longitud":"-75.473853","latitud":"5.036146","direccion":"Cra 31 Via Enea","imagen":"_files\/escenario\/img\/20140803122230000000-__-bosque.jpg","telefono":"8890000","encargado":"Arnulfo Guar\u00edn"},{"id":"1","nombre":"Coliseo Mayor","longitud":"-75.487366","latitud":"5.057593","direccion":"Cra 23 # 63 - 18","imagen":"_files\/escenario\/img\/20140803122230000000-__-coliseo.jpeg","telefono":"8890000","encargado":"Mart\u00edn Munevar"}]';
 	listaEscenarios =eval(listaEscenarios);
@@ -341,5 +375,87 @@ function showMarkers() {
 	
   }
   
+  
+  /*listar eventos*/
+  function listarEventos(){
+	markers = [];
+	//servicio retorna este json
+	var listaEventosCiudad = '[{"id":"2","titulo":"Torneo de Ultimate Femenino","fecha":"2014-08-22","hora":"10:00","descripcion":"Torneo Femenino de Ultimate que se llevar\u00e1 a cabo en las instalaciones del bosque popular cerca a la cafeter\u00eda.","telefono":"8890000","email":"contacto@correo.com","escenario-nombre":"Bosque Popular","escenario-latitud":"5.036146","escenario-longitud":"-75.473853","escenario-direccion":"Cra 31 Via Enea"},{"id":"1","titulo":"Caminata Campa\u00f1a Contra el Cancer de mama","fecha":"2014-08-08","hora":"09:00","descripcion":"Marcha para tomar conciencia de los cuidados para combatir el c\u00e1ncer de mama.","telefono":"8890000","email":"contacto@correo.com","escenario-nombre":"Coliseo Mayor","escenario-latitud":"5.057593","escenario-longitud":"-75.487366","escenario-direccion":"Cra 23 # 63 - 18"}]';
+	listaEventosCiudad = eval(listaEventosCiudad.replace(/-/g, ""));	
+	geocoder = new google.maps.Geocoder();
+
+     var address = "manizales";
+
+     geocoder.geocode({
+         'address': address
+     }, function (results, status) {
+
+         if (status == google.maps.GeocoderStatus.OK) {
+
+             var latitude = results[0].geometry.location.lat();
+             var longitude = results[0].geometry.location.lng();
+             //alert(latitude);
+             //alert(longitude);
+
+             var latlng = new google.maps.LatLng(latitude, longitude);
+
+             var mapOptions = {
+                 zoom: 12,
+                 center: latlng,
+                 mapTypeId: google.maps.MapTypeId.ROADMAP
+             }
+
+             mapEventos = new google.maps.Map(document.getElementById('map-container-events'), mapOptions);
+
+             var latlng = new google.maps.LatLng(latitude, longitude);
+             mapEventos.setCenter(latlng);
+			
+			var image = 'img/puntoActual.png';	
+             var marker = new google.maps.Marker({
+                 map: mapEventos,
+                 position: latlng,
+                 title: 'Mi ubicacion!',
+				 draggable:true,
+				 animation: google.maps.Animation.DROP,
+				 icon: image
+
+             });
+			 //console.log(eventosCiudad);
+			 google.maps.event.addListener(marker, 'click', toggleBounce);
+			 
+				//console.log(listaEventos.length)
+				$.each(listaEventosCiudad, function (indice, valor) {	
+					//console.log(valor.escenariolatitud)
+					setTimeout(function() {
+					
+					var imageIcon2 = 'img/puntoEvento.png';	
+				  
+					var marker = new google.maps.Marker({
+						position: new google.maps.LatLng(valor.escenariolatitud,"-"+valor.escenariolongitud),
+						map: mapEventos,
+						draggable: false,
+						animation: google.maps.Animation.DROP,
+						icon: imageIcon2,
+						clickable: true
+					  })
+					marker.info = new google.maps.InfoWindow({
+					  content: '<b>' + valor.titulo + '</b></br> ' + valor.escenariodireccion
+					});
+					google.maps.event.addListener(marker, 'click', function() {
+						this.info.open(mapEventos, marker);
+					});
+					
+				  markers.push(marker);
+				 
+				}, indice * 500);
+			});
+		}
+			 
+
+     });
+	 
+	
+	
+  }
 	
 	
